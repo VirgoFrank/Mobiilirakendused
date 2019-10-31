647d2cc4f2ae68616d42d6e7b03f4b46841e05e7
VirgoFrank <48998271+VirgoFrank@users.noreply.github.com>
2019-10-31 09:47:16 +0200

    star wars app

---
+++ StarwarsApp/StarwarsApp.Core/DataServicecs.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarwarsApp.Core
+{
+    public class DataService
+    {
+        public static async Task<People> GetStarWarsPeople(string queryString)
+        {
+            HttpClient client = new HttpClient();
+            var response = await client.GetStringAsync(queryString);
+
+            People data = null;
+            if (response != null)
+            {
+                data = JsonConvert.DeserializeObject<People>(response);
+            }
+            return data;
+        }
+    }
+}
---
+++ StarwarsApp/StarwarsApp/MainActivity.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Widget;
+using Android.Content;
+using StarwarsApp;
+using StarwarsApp.Core;
+
+namespace StarwarsApp
+{
+    [Activity(Label = "string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+           
+
+           
+            
+            var searchfield = FindViewById<EditText>(Resource.Id.searchEditText);
+            var searchButton = FindViewById<Button>(Resource.Id.searchButton);
+            var peopleListView = FindViewById<ListView>(Resource.Id.peopleListView);
+           
+            searchButton.Click += async delegate
+            {
+                var searchText = searchfield.Text;
+                var queryString = "https://swapi.co/api/people/?search=" + searchText;
+                var data = await DataService.GetStarWarsPeople(queryString);
+                peopleListView.Adapter = new StarWarsPeopleAdapter(this, data.Results);
+               
+            };
+             
+        }
+
+
+
+      
+      
+
+       
+
+       
+    }
+}
+
