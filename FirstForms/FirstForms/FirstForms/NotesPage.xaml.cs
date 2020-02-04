6ad431eb0d5de910821e3216c1c0437e90d5a486
Tehnikumi Opilane <opilane@tptlive.ee>
2020-02-04 09:52:12 +0200

    tunnitöö

---
+++ FirstForms/FirstForms/FirstForms/NotesPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using FirstForms.Models;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstForms
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class NotesPage : ContentPage
+	{
+		public NotesPage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override async void OnAppearing()
+        {
+            base.OnAppearing();
+
+            NotesListView.ItemsSource = await App.Database.GetNotesAsync();
+        }
+
+        async void OnNoteAddedClicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new NoteEntryPage
+            {
+                BindingContext = new Note()
+            });
+        }
+
+        async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            if(e.SelectedItem != null)
+            {
+                await Navigation.PushAsync(new NoteEntryPage
+                {
+                    BindingContext = e.SelectedItem as Note
+                });
+            }
+        }
+
+
+    }
+}
